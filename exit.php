7edc4ad692de3003c37fac829196fa170d999ad8
alexkart-hub <57880131+alexkart-hub@users.noreply.github.com>
2019-11-20 10:45:30 +0300

    Добавил figure в базу данных, вывел на экран. Вход и выход работает

--- exit.php
+++ exit.php
@@ -1,4 +1,7 @@
 <?php
+$connect = mysqli_connect('localhost','root','','lessprojectbase');
+$query = "INSERT INTO users (figure) VALUES ('$user_figure')";
+mysqli_query($connect,$query);
 unset($_COOKIE['user_id']);
 unset($_COOKIE['username']);
 setcookie('user_id','',-1,'/');
